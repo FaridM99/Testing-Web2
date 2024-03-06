cca2ff3fa55447f72e832952b22a52c6b81bdceb
crypteex <mehdi_2002@list.ru>
2024-03-06 22:54:16 +0400

    Issues

---
+++ TestingCrud/src/main/java/com/TestingCrud/TestingCrud/TestingCrudApplication.java
@@ -0,0 +1,13 @@
+package com.TestingCrud.TestingCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingCrudApplication.class, args);
+	}
+
+}
---
+++ TestingCrud/src/test/java/com/TestingCrud/TestingCrud/TestingCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TestingCrud.TestingCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
