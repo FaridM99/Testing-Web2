e7a482707ac0e72fec362ef424f192818bb33bb6
crypteex <mehdi_2002@list.ru>
2024-03-06 23:09:59 +0400

    Updated Readme File

--- TestingCrud/src/main/java/com/TestingCrud/TestingCrud/TestingCrudApplication.java
+++ TestingCrud/src/main/java/com/TestingCrud/TestingCrud/TestingCrudApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-// Initial Commit on IDEA
+// Initial Commit on IDEA (testing)
 
 @SpringBootApplication
 public class TestingCrudApplication {
